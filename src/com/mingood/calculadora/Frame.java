16f75ee9baa97e8cb87fc506b7459d0ca0f3eb4b
JoseDom18 <199011605+JoseDom18@users.noreply.github.com>
2025-10-21 23:51:58 -0600

    primera parte hecha

--- src/com/mingood/calculadora/Frame.java
+++ src/com/mingood/calculadora/Frame.java
@@ -13,17 +13,18 @@ public class Frame extends JPanel {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	private JButton result = new JButton();
-	JPanel frame2;
+	private JButton result = new JButton("0");
+	private JPanel frame2;
+	private boolean begin;
 
 	public Frame() {
+		begin = true;
 		setLayout(new BorderLayout());
 		result.setEnabled(false);
 		add(result, BorderLayout.NORTH);
-		result.setText("0");
 
 		frame2 = new JPanel();
-		frame2.setLayout(new GridLayout(4 , 5));
+		frame2.setLayout(new GridLayout(4, 5));
 		putButton("7");
 		putButton("8");
 		putButton("9");
@@ -45,21 +46,25 @@ public class Frame extends JPanel {
 		putButton("Ans");
 		putButton("=");
 		add(frame2, BorderLayout.CENTER);
-		
+
 	}
 
-	private void putButton (String r) {
-		
+	private void putButton(String r) {
+
 		JButton button = new JButton(r);
-		
+
 		button.addActionListener(e -> {
-			
+
+			if (begin) {
+				result.setText("");
+				begin = false;
+			}
+
 			result.setText(result.getText() + e.getActionCommand());
-			
+
 		});
-		
+
 		frame2.add(button);
 	}
 
-	
 }
